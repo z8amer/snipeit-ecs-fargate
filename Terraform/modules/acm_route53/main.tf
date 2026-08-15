e0147a4087a9e201fdf0fafcf37cc5b11ecc9937
z8amer <z8amer@://github.com>
2026-08-15 10:38:08 +0000

    fix: added 'allow_overwrite' to Route53 alias record

--- Terraform/modules/acm_route53/main.tf
+++ Terraform/modules/acm_route53/main.tf
@@ -47,6 +47,7 @@ resource "aws_route53_record" "alb_alias" {
   zone_id = data.aws_route53_zone.primary_zone.zone_id
   name    = "${var.sub_domain}.${var.root_domain}"
   type    = "A" # Route 53 alias records must always be type A
+  allow_overwrite = true
 
   alias {
     name                   = var.alb_dns_name
