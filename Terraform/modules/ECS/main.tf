06394e2689a554a3b2019c0c134ecd4ccdb2fb1c
z8amer <z8amer@://github.com>
2026-08-16 14:40:24 +0000

    amend: hardcoded APP_ENV & APP_KEY to see if Laravel ingests them

--- Terraform/modules/ECS/main.tf
+++ Terraform/modules/ECS/main.tf
@@ -36,11 +36,11 @@ resource "aws_ecs_task_definition" "app_task" {
 
       environment = [
         {
-          name  = var.app_env_key_name
+          name  = "APP_ENV"
           value = var.app_env
         },
         {
-          name  = var.app_key_name
+          name  = "APP_KEY"
           value = "${"base64:"}${base64encode(random_password.laravel_app_key.result)}"
         }
       ]
